a12a2a80cbe6bbf745d516d6c09028da762635fa
KennethFelix03 <72651173+KennethFelix03@users.noreply.github.com>
2020-10-17 18:26:59 +0800

    value of i change into 1 value of j change into 24-1 and value of k change into 1

--- TRI2.C
+++ TRI2.C
@@ -1,20 +1,21 @@
 #include<stdio.h>
+#include<conio.h>
 
 void main()
+{  int i, j, k;
+   clrscr();
+   for(i = 1; i < 24; i++)
 {
-  int i,j, k;
-  clrscr();
+   for(j = 24-1; j >= i; j--)
+{
+   printf(" ");
+   }
+   for(k = 1; k <= i; k++)
+   printf("*");
+   printf("\n");
 
-  for(i = 0; i < 24; i++);
-  {
-     for(j = i; j >= 0; j--);
-     {
-	printf(" ");
-     }
-     for(k = 0; k <= i; k++)
-       printf("*");
-     printf("\n");
-  }
-
-  getch();
 }
+   getch();
+}
+
+
